1935553c749456512d2e36ba3a58cd9d1887bbb5
Ayush Singh <45001309+adamcode26@users.noreply.github.com>
2021-07-03 19:41:24 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
+const port = process.env.PORT||3000
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const { error } = require('console')
@@ -83,6 +84,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen('3000',()=> {
+app.listen(port,()=> {
     console.log("App is running")
 })
